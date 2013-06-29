595f50073ee6e655461a8b9a1e3b9026ff1b613f
Kosmosfans <kosmosfans@gmail.com>
2013-06-29 17:56:36 +0800

    first commit

---
+++ Dummy2/src/com/focus/dummy/Dummy2.java
@@ -0,0 +1,14 @@
+package com.focus.dummy;
+
+public class Dummy2 {
+
+	public Dummy2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("this is %d", 100);
+	}
+
+}
