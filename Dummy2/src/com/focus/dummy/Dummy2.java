b0eb1111aa19c314a4f7449a09b2f34761e1d30f
Kosmosfans <kosmosfans@gmail.com>
2013-06-29 17:59:49 +0800

    2

--- Dummy2/src/com/focus/dummy/Dummy2.java
+++ Dummy2/src/com/focus/dummy/Dummy2.java
@@ -9,6 +9,10 @@ public class Dummy2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.printf("this is %d", 100);
+		System.out.printf("this is %d", 100);
+		System.out.printf("this is %d", 100);
+		System.out.printf("this is %d", 100);
+		System.out.printf("this is %d", 100);
 	}
 
 }
